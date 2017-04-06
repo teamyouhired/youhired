8e7c7d5d132edc9184d2f597fd9e4729c2b62a55
Ryan Freude <freudehack@gmail.com>
2017-04-06 15:33:38 -0700

    installed react router, redux promise, added routes file updated app

---
+++ app/routes.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Route, IndexRoute } from 'react-router';
+
+import _Root from './components/_Root';
+
+//list all the routes and what components should be shown
+export default (
+<Route path="/" component={_Root}/>
+);
