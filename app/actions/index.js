0ec85309f825adcf1e2da202bcf567c73f750554
Ryan Freude <freudehack@gmail.com>
2017-04-06 16:39:03 -0700

    installed axios for requests

--- app/actions/index.js
+++ app/actions/index.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 export function actionName(element) {
   //need to define an action creator and needs to return an action
   //an object with a type property, usually has a type(purpose), and payload(data that describes action)
