209cd775d7e3b58b16f89fe0ee43a0471a510af1
Jesse Hill <jsnahill@gmail.com>
2017-04-07 10:17:17 -0700

    Fix issue where reducer was not a function.

--- app/reducers/reducers.jsx
+++ app/reducers/reducers.jsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-
 import App from '../app';
 
 export default function() {
