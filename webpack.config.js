2a8e915f5e4230be8648a36cba4eb5c1902839b9
Jesse Hill <jsnahill@gmail.com>
2017-04-07 10:12:09 -0700

    Added react-hot-loader.

--- webpack.config.js
+++ webpack.config.js
@@ -1,65 +1,67 @@
-const path = require('path');
-// const ExtractTextPlugin = require('extract-text-webpack-plugin');
-// console.log('path', __dirname + './app/app.jsx');
-console.log('path', path.resolve(__dirname, './app/actions'));
-console.log('path', path.resolve(__dirname, 'app/actions'));
-
-console.log('path.resolve(__dirname, app)', path.resolve(__dirname, 'app'));
+var path = require('path');
+var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  entry: './app/app.jsx',
-  // entry: path.resolve(__dirname, './app/app.jsx'),
-  output: {
-    path: __dirname,
-    filename: './public/bundle.js'
-  },
-  resolve: {
-    alias: {
-    },
-    // when doing 'require' and 'import', webpack will check this directories for files
-    // e.g. import ... from 'filename', no need to specify path or file extensions
-    modules: [
-      'app/actions',
-      'app/components',
-      'app/containers',
-      'app/reducers',
-      'styles',
-      'node_modules'
+    devtool: 'inline-source-map',
+    entry: [
+      'webpack-dev-server/client?http://127.0.0.1:8080/',
+      'webpack/hot/only-dev-server',
+      './app/app.jsx'
     ],
-    extensions: ['.js', '.jsx', '.scss']
-  },
-
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['react', 'es2015', 'stage-2']
-          }
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js'
+    },
+    resolve: {
+        alias: {
+          app: path.resolve(__dirname, 'app')
+        },
+        modules: [
+          'node_modules'
+        ],
+        extensions: ['.js', '.jsx', '.scss']
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          use: [
+            'react-hot-loader',
+            'babel-loader?presets[]=react,presets[]=es2015,presets[]=stage-2'
+          ]
+        },
+        {
+          test: /\.scss$/,
+          use: ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: [
+              'css-loader',
+              'autoprefixer-loader?browsers=last 3 versions',
+              'sass-loader?outputStyle=expanded'
+            ]
+          })
+        },
+        {
+          test: /\.(jpe?g|png|gif|svg)$/i,
+          use: [
+            'url-loader?limit=8192',
+            'img-loader'
+          ]
         }
+      ]
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.NoEmitOnErrorsPlugin(),
+      new ExtractTextPlugin("styles.css")
+    ],
+    devServer: {
+      hot: true,
+      proxy: {
+        '*': 'http://127.0.0.1:' + (process.env.PORT || 3000)
       },
-      {
-        test: /\.scss$/,
-        use: [
-          'style-loader',
-          // allows to use @import statements
-          'css-loader',
-          {
-            loader: 'postcss-loader',
-            options: {
-              plugins: function () {
-                return [ require('autoprefixer')];
-              }
-            }
-          },
-          // converts SASS/SCSS to CSS
-          'sass-loader'
-        ]
-      }
-    ]
-  },
-  devtool: 'cheap-module-eval-source-map'
+      host: '127.0.0.1'
+    }
 };
