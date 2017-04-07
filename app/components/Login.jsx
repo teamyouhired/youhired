ef2d7af9318833ac7b6b86b69af7872adb9e3695
Jesse Hill <jsnahill@gmail.com>
2017-04-07 10:11:17 -0700

    Initial work on login page.

---
+++ app/components/Login.jsx
@@ -0,0 +1,28 @@
+import React, { createClass } from 'react';
+
+const Login = createClass({
+
+  signUserIn(email, password) {
+    dispatch({
+      type: 'SIGN_IN',
+      email,
+      password
+    });
+  }
+  render() {
+    return (
+      <div>
+        <form onSubmit={signUserIn(this.email, this.password)}>
+          <input
+            ref={email => this.email = email}
+            placeholder="Email"
+          />
+          <input
+            ref={password => this.password = password}
+            placeholder="Password"
+          />
+        </form>
+      </div>
+    );
+  }
+});
