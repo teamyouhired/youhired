326be4e31da6981ddcad9a074222a0ef9c6721db
Jesse Hill <jsnahill@gmail.com>
2017-04-08 11:03:19 -0700

    Initial changes to routing, now using react-router-dom instead of react-router.

--- app/app.jsx
+++ app/app.jsx
@@ -1,36 +1,80 @@
+// import React, { PropTypes } from 'react';
+// import ReactDOM from 'react-dom';
+// import ReduxPromise from 'redux-promise';
+// import { Provider } from 'react-redux';
+// import { combineReducers, createStore, applyMiddleware } from 'redux';
+// //research browser history, hash history, memory history...which might be more useful
+// import { Router, browserHistory } from 'react-router';
+// import path from 'path';
+
+// import routes from './routes';
+// import jobsList from './reducers/reducers';
+// import _Root from './components/_Root';
+
+// // load scss styles
+// // require('app.scss');
+
+// const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
+
+// ReactDOM.render(
+//   <Provider store={createStoreWithMiddleware(jobsList)}>
+//     <_Root />
+//   </Provider>
+//   , document.getElementById('root'));
+
+//==========================================================
+
+//uncomment below code when we are ready to implement react-router
+//can be left commented for now...
+// ReactDOM.render(
+//   <Provider store={createStoreWithMiddleware(jobsList)}>
+//     <Router history={browserHistory} routes={routes} />
+//   </Provider>
+//   , document.querySelector('.container'));
+
+/********** testing routing **********/
+
+
 import React, { PropTypes } from 'react';
 import ReactDOM from 'react-dom';
 import ReduxPromise from 'redux-promise';
 import { Provider } from 'react-redux';
 import { combineReducers, createStore, applyMiddleware } from 'redux';
 //research browser history, hash history, memory history...which might be more useful
-import { Router, browserHistory } from 'react-router';
 import path from 'path';
+import { browserHistory } from 'react-router';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import routes from './routes';
 import jobsList from './reducers/reducers';
 import _Root from './components/_Root';
 
-// load scss styles
-//require('app.scss');
-
-// probably not the best method but works for now
-// const allReducers = combineReducers(reducers);
-
 const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
-ReactDOM.render(
+ReactDOM.render (
   <Provider store={createStoreWithMiddleware(jobsList)}>
-    <_Root />
-  </Provider>
-  , document.getElementById('root'));
+    <Router history={browserHistory}>
+      <Route exact path='/' component={_Root}/>
+    </Router>
+  </Provider>,
+    document.getElementById('root')
+);
 
-//==========================================================
 
-//uncomment below code when we are ready to implement react-router
-//can be left commented for now...
-/*ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <Router history={browserHistory} routes={routes} />
-  </Provider>
-  , document.querySelector('.container'));*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app/routes.js
+++ app/routes.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import { Route, IndexRoute } from 'react-router';
+// import React from 'react';
+// import { Route, IndexRoute } from 'react-router';
 
-import _Root from './components/_Root';
+// import _Root from './components/_Root';
 
-//list all the routes and what components should be shown
-export default (
-<Route path="/" component={_Root}/>
-);
+// //list all the routes and what components should be shown
+// export default (
+// <Route path="/" component={_Root}/>
+// );
+
+/********** testing routing **********/
+
