a3d889eddef85769af3efd757dbfab0ecd9cec1f
Dimitri Temkin <dimitri.temkin@gmail.com>
2017-04-12 10:26:08 +1000

    Change db connection

--- server/db.js
+++ server/db.js
@@ -6,6 +6,10 @@ var Sequelize = require('sequelize');
 //   dialect: 'postgres'
 // });
 
+// module.exports = new Sequelize('youhired', 'macuser', 'password', {
+//   dialect: 'postgres'
+// });
+
 //CONNECTION TO HEROKU DATABASE
 var DATABASE_URL = 'postgres://qraqhctfxpyoal:c2768c79581f2001d90e70be012f34f8be5de141bf6eeb8b2f606e845f6700e4@ec2-54-83-48-188.compute-1.amazonaws.com:5432/d2b5jjjej0pc8g';
 
