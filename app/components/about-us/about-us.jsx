fcc6784142362f9424643ec8918238019080e384
Ryan Freude <freudehack@gmail.com>
2017-04-14 06:39:56 -0700

    updated some css, created new divs for more functionality

--- app/components/about-us/about-us.jsx
+++ app/components/about-us/about-us.jsx
@@ -9,43 +9,44 @@ const About = createClass({
         <div className="row">
             <div className="col-lg-12">
                 <h1 className="page-header">About Us
-                    <small>It's Nice to Meet You!</small>
                 </h1>
                 <p>youHired is a personal job search Organizer. An app where a job seeker can save and track job application status, set reminders, take notes,set search goals, document individual interview restrospectives and save contact info.</p>
             </div>
         </div>
 
-        <div className="row">
-            <div className="col-lg-12">
+        <div className="text-center">
+            <div>
                 <h2 className="page-header">Our Team</h2>
             </div>
-            <div className="col-lg-4 col-sm-6 text-center">
-                <img className="img-circle img-responsive img-center" src="http://placehold.it/200x200" alt="" />
-                <h3>Dimitri Timkin
-                    <small>Product Owner</small>
-                </h3>
-                <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
-            </div>
-            <div className="col-lg-4 col-sm-6 text-center">
-                <img className="img-circle img-responsive img-center" src="http://placehold.it/200x200" alt="" />
-                <h3>Chris Abrami
-                    <small>Scrum Master</small>
-                </h3>
-                <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
-            </div>
-            <div className="col-lg-4 col-sm-6 text-center">
-                <img className="img-circle img-responsive img-center" src="http://placehold.it/200x200" alt="" />
-                <h3>Ryan Freude
-                    <small>Software Engineer</small>
-                </h3>
-                <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
-            </div>
-            <div className="col-lg-4 col-sm-6 text-center">
-                <img className="img-circle img-responsive img-center" src="http://placehold.it/200x200" alt="" />
-                <h3>Jesse Hill
-                    <small>Software Engineer</small>
-                </h3>
-                <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
+            <div className="text-center">
+                <div className="col-lg-4 col-sm-6 text-center team-card">
+                    <img className="img-circle img-responsive img-center" src="http://placehold.it/200x200" alt="" />
+                    <h3>Dimitri Timkin
+                        <small>Product Owner</small>
+                    </h3>
+                    <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
+                </div>
+                <div className="col-lg-4 col-sm-6 text-center team-card">
+                    <img className="img-circle img-responsive img-center" src="http://placehold.it/200x200" alt="" />
+                    <h3>Chris Abrami
+                        <small>Scrum Master</small>
+                    </h3>
+                    <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
+                </div>
+                <div className="col-lg-4 col-sm-6 text-center team-card">
+                    <img className="img-circle img-responsive img-center" src="http://placehold.it/200x200" alt="" />
+                    <h3>Ryan Freude
+                        <small>Software Engineer</small>
+                    </h3>
+                    <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
+                </div>
+                <div className="col-lg-4 col-sm-6 text-center team-card">
+                    <img className="img-circle img-responsive img-center" src="http://placehold.it/200x200" alt="" />
+                    <h3>Jesse Hill
+                        <small>Software Engineer</small>
+                    </h3>
+                    <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
+                </div>
             </div>
         </div>
 
