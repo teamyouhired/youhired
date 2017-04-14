fadf77c613c150386838bea3a29149f81daa74ea
Chris Abrami <abramicf@gmail.com>
2017-04-14 12:11:15 -0700

    Most basic version of refactored algorithm works

--- server/controllers/generalQueries.js
+++ server/controllers/generalQueries.js
@@ -12,45 +12,77 @@ var ContactApplicationJoin = require('./../models/ContactApplicationJoinModel');
 module.exports = {
 
   getData: function (req, res) {
-    console.log('youre in!');
+    // console.log('youre in!');
 
-    var applicationids = [];
+    // var applicationids = [];
 
     var data = {
-      contacts: 0,
-      jobApplications: []
     };
 
-    Contact.findAll({
-      where: {
-        seeduserid: req.body.seeduserid
-      }
-    }).then(function(results) {
-      data.contacts = results;
-    }).then(function(){
-
-      JobApplication.findAll({
-        // attributes: ['id'],
+    var getContacts = function(){
+      return Contact.findAll({
         where: {
           seeduserid: req.body.seeduserid
         }
-      }).then(function(results){
-        for(var i = 0; i < results.length; i++){
-          data.jobApplications.push({
-            applicationdetails: results[i].dataValues
-          });
-          applicationids.push(results[i].dataValues['id']);
-        }
-      }).then(function(){
-        res.send(data);
-        console.log(applicationids);
       });
+    }
 
-     }).error(function(err) {
-      console.log('oh no', err);
+    var getApplications = function(){
+      return JobApplication.findAll({
+        where: {
+          seeduserid: req.body.seeduserid
+        }
+      });
+    }
+
+
+    var getActivitiesForApplication = function(){
+      return JobApplication.findAll({
+        where: {
+          seedapplicationid: id
+        }
+      });
+    }
+
+
+    //subroutine occurs after 'getApplications'
+    var subroutine = function(info, counter = 0){
+      //if the counter is equal to info.length then return
+      if(counter === info.length){
+        return;
+      } else {
+        //set the variable for seedapplicationid
+        var id = info[counter]['dataValues']['seedApplicationId'];
+        //run the getActivitiesQuery
+        getActivitiesForApplication()
+        .then(function(result){
+          data['jobapplications'][counter]['activities'] = result.dataValues || [];
+        })
+        .then(subroutine(info, counter ++));
+        //then, push the result to applicationactivities in data
+        //then, run again to counter++
+      }
+    }
+
+    getContacts()
+    .then(function(results){
+      data['contacts'] = results;
+    }).then(function(){
+      res.send(data);
     });
+
   }
 };
+
+
+
+
+
+//we can just stick then on to the back
+//
+
+
+
 
 
 
@@ -80,6 +112,52 @@ module.exports = {
 
 
 
+//PREIVOUS FUNCTION
 
+// Contact.findAll({
+//       where: {
+//         seeduserid: req.body.seeduserid
+//       }
+//     }).then(function(results) {
+//       data.contacts = results;
+//     }).then(function(){
+
+//         JobApplication.findAll({
+//         // attributes: ['id'],
+//           where: {
+//             seeduserid: req.body.seeduserid
+//           }
+//         }).then(function(results){
+//           results.forEach(function(item, index) {
+//             return new Promise(function(resolve, reject){
+
+//             data.jobapplications.push({
+//               applicationdetails: item.dataValues
+//             })
+//             // console.log(data.jobapplications[i]);
+//             var id = item.dataValues['seedapplicationid'];
+//             console.log(id);
+//           }).then(function(){
+//             //it's as if this is starting AND retrieving BEFORE the 'push' happens above
+//             ActivityLog.findAll({
+//                 where: {
+//                   seedapplicationid: id
+//                 }
+//                 }).then(function(results){
+//                     console.log('youre in! ' + results);
+//                     // console.log('Activities: ' + data.jobapplications[index]['activities']);
+//                     data.jobapplications[index]['activities']: results || 0;
+//                 })
+//               })
+//             })//loop ends here
+//           }).then(function(){  //when the loop is complete, send along the data!
+//             res.send(data);;
+//                 console.log(data)
+
+//         })
+//      }).error(function(err) {
+//       console.log('oh no', err);
+//     });
+//   }
 
 
