729f050857e87ed9c57937ee846ea925132461a0
Jesse Hill <jsnahill@gmail.com>
2017-04-21 17:23:35 -0700

    Added date and time picker to interview form.

--- app/components/application-forms/InterviewForm.jsx
+++ app/components/application-forms/InterviewForm.jsx
@@ -1,30 +1,53 @@
 import React, { createClass } from 'react';
+import 'react-date-picker/index.css';
+import { TransitionView, Calendar } from 'react-date-picker';
+import moment from 'moment';
 
 const InterviewForm = createClass({
+  displayName: 'InterviewForm',
+
+  getInitialState () {
+    return {
+      startDate: moment(),
+      dateString: ''
+    }
+  },
 
   cancelForm() {
-
     this.companyAddress.value = '';
-
     this.props.hideModal();
   },
 
   onSubmit(event) {
     event.preventDefault();
+    console.log('date being sent to database', this.state.dateString);
     this.props.addInterview({
       applicationid: this.props.applicationId,
       companyaddress:  this.companyAddress.value,
+      datestring: this.state.dateString
     });
 
     this.companyAddress.value = '';
-
     this.props.hideModal();
+  },
+
+  handleChange(dateString, { dateMoment }) {
+    this.setState({
+      startDate: dateMoment,
+      dateString: dateString
+    });
   },
 
   render() {
     return (
       <form onSubmit={this.onSubmit}>
-
+        <TransitionView>
+          <Calendar
+            dateFormat="DD/MM/YYYY HH:mm"
+            defaultDate={this.state.startDate}
+            onChange={this.handleChange}
+          />
+        </TransitionView>
         <textarea
           className='form-control'
           ref={companyAddress => { this.companyAddress = companyAddress }}
