9991d6fdd68d678df60a57ae413f48f3be3c0a04
Ryan Freude <freudehack@gmail.com>
2017-04-26 16:08:19 -0700

    pushed up chart for data entry

--- app/components/dashboard/visualdata/VisualData.jsx
+++ app/components/dashboard/visualdata/VisualData.jsx
@@ -1,43 +1,46 @@
 import React, { createClass, PropTypes } from 'react';
 import { browserHistory } from 'react-router';
-import { LineChart, Line } from 'recharts';
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts'
-import { PieChart, Pie, Sector, Cell } from 'recharts'
+import { LineChart, Line, ResponsiveContainer } from 'recharts';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts';
+import { PieChart, Pie, Sector, Cell } from 'recharts';
 
-const data1 = [{name: 'Group A', value: 400}, {name: 'Group B', value: 300},
-                  {name: 'Group C', value: 300}, {name: 'Group D', value: 200}];
 const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
 
-const RADIAN = Math.PI / 180;
-
 const data = [
-      {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
-      {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
-      {name: 'Page C', uv: 2000, pv: 9800, amt: 2290},
-      {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
-      {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
-      {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
-      {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
+      {name: 'Interview', Current: 8, Goal: 30},
+      {name: 'Info', Current: 41, Goal: 100},
+      {name: 'Applied', Current: 308, Goal: 350},
+      {name: 'Interested', Current: 401, Goal: 500},
 ];
+
+// const data1 = [
+//       {name: 'Interview', Total: 8, amt: 4},
+//       {name: 'Info', Total: 41, amt: 12},
+//       {name: 'Applied', Total: 308, amt: 19},
+//       {name: 'Interested', Total: 401, amt: 300},
+// ];
 
 const VisualData = createClass({
   displayName: 'VisualData',
 
   render() {
     return (
-      <ResponsiveContainer width="100%">
+
+        <ResponsiveContainer width="95%" height="100%">
           <BarChart width={450} height={250} data={data}>
             <XAxis dataKey="name" />
             <YAxis />
-            <CartesianGrid strokeDasharray="3 3" />
+            <CartesianGrid strokeDasharray="5 4" />
             <Tooltip />
             <Legend />
-            <Bar dataKey="pv" fill="#8884d8" />
-            <Bar dataKey="uv" fill="#82ca9d" />
+            <Bar dataKey="Current" fill="#8884d8" />
+            <Bar dataKey="Goal" fill="#FF8042" />
           </BarChart>
-      </ResponsiveContainer>
+        </ResponsiveContainer>
+
     );
   }
 });
 
 export default VisualData;
+
